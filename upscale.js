23c50fe6687031025bed17bd378a7b158d2941cb
Bastien Bras de fer <bastien.bras-de-fer@epitech.eu>
2020-12-07 15:33:52 +0100

    Better resize handling

--- upscale.js
+++ upscale.js
@@ -1,21 +1,28 @@
 const video = document.getElementsByTagName('video')[0];
+video.style.visibility = 'hidden';
+const ratio = video.videoHeight / video.videoWidth;
+
 const canvas = document.createElement('canvas');
+canvas.style.position = 'absolute';
 const ctx = canvas.getContext('2d', { alpha: false });
 
 let width, height, size;
-(onresize = video.onfullscreenchange = () => {
+function resize() {
     width = canvas.width = video.offsetWidth;
-    height = canvas.height = Math.floor(video.videoHeight * width / video.videoWidth);
+    height = canvas.height = Math.floor(width * ratio);
     size = width * height * 4;
-})();
-video.style.visibility = 'hidden';
-canvas.style.position = 'absolute';
+}
+
+new ResizeObserver(resize).observe(video), resize();
 video.after(canvas);
 
 (function step() {
-    ctx.drawImage(video, 0, 0, width, height);
-    let frame = ctx.getImageData(0, 0, width, height);
-    for (let i = 0; i < size; i += 4) frame.data[i] = 255;
-    ctx.putImageData(frame, 0, 0);
     requestAnimationFrame(step);
+
+    if (width && height) {
+        ctx.drawImage(video, 0, 0, width, height);
+        let frame = ctx.getImageData(0, 0, width, height);
+        for (let i = 0; i < size; i += 4) frame.data[i] = 255;
+        ctx.putImageData(frame, 0, 0);
+    }
 })();
