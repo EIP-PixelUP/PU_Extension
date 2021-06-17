1a94cdb2e6f09a1baa4a01ce846c8528970de780
Alexis Crozet <alexis.crozet@epitech.eu>
2021-06-17 10:57:53 +0200

    feat(packager): tweaked error messages to enhance clarity

--- package.sh
+++ package.sh
@@ -4,6 +4,6 @@ echo "Packaging extension for Firefox..."
 
 web-ext --version > /dev/null || (echo "You need to install web-ext first" && exit 1)
 
-cd out || (echo "Couldn't find the 'out' directory." && exit 1)
+cd out || (echo "Couldn't find the 'out' directory, try to build first." && exit 1)
 
 path=`web-ext build --overwrite-dest`
